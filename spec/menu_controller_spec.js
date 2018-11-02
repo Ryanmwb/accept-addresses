b6b6f8aa46aaf7dc3f8d3d3b31735e929c4c8f3e
Ryan <ryanmwb@gmail.com>
2018-11-02 14:28:20 -0400

    finish creating contacts and connecting to a database checkpoint

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -7,19 +7,4 @@ describe("MenuController", () => {
             expect(MenuController).toBeDefined();
         });
     });
-    describe("#getContactCount()", () => {
- // #2
-        beforeEach(() => {
-            this.menu = new MenuController();
-        });
-
-        it("should return 0 when no contacts are in the book", () => {
-            expect(this.menu.getContactCount()).toBe(0);
-        });
-
-        it("Should return 1 when there is exactly one contact in the book", () => {
-            this.menu.book.addContact("Bob", "555-555-5555");
-            expect(this.menu.getContactCount()).toBe(1)
-        });
-    });
 });
